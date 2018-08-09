e482aba5e76d70d20a4ba475b2cb991be1ffa1b1
zpalmer <zoepalmer2014@u.northwestern.edu>
2018-08-09 14:19:37 -0500

    Refactor Tumblr, fix bug in client response handling, and handle longer requests

--- src/main/java/zpalmer/tumbldown/client/Tumblr.java
+++ src/main/java/zpalmer/tumbldown/client/Tumblr.java
@@ -16,49 +16,54 @@ public class Tumblr {
     private static final int LIMIT = 50;
     private static final int TUMBLR_LIMIT = LIMIT - 1; // because they zeroth this :|
 
-    private WebTarget baseTarget;
 
     private Client client;
 
     public Tumblr(Client client) {
         this.client = client;
-        this.baseTarget = client.target(BASE_API_URL)
-                .queryParam("api_key", AUTHENTICATION);
     }
+
+    // need to handle exceptions thrown by the client
 
     public TumblrResponse getBlog(String name) {
         // api.tumblr.com/v2/blog/{blog-identifier}/info?api_key={key}
-        WebTarget target = baseTarget.path("blog/" + name + ".tumblr.com/" + "info");
+        WebTarget target = target("blog/" + name + ".tumblr.com/" + "info");
         Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
 
-        Response response = invocationBuilder.get();
-
-        return respondToSuccessOrFailure(response);
+        return makeRequestAndRespondToSuccessOrFailure(invocationBuilder);
     }
 
     public TumblrResponse getLikes(String blogName, long beforeTime) {
         //api.tumblr.com/v2/blog/{blog-identifier}/likes?api_key={key}
-
-        WebTarget likesTarget = baseTarget.path("blog/" + blogName + ".tumblr.com/" + "likes")
+        WebTarget likesTarget = target("blog/" + blogName + ".tumblr.com/" + "likes")
                 .queryParam("limit", TUMBLR_LIMIT)
                 .queryParam("before", beforeTime);
 
         Invocation.Builder invocationBuilder = likesTarget.request(MediaType.APPLICATION_JSON);
-        Response response = invocationBuilder.get();
 
-        return respondToSuccessOrFailure(response);
+        return makeRequestAndRespondToSuccessOrFailure(invocationBuilder);
     }
 
-    private TumblrResponse respondToSuccessOrFailure(Response response) {
+    private TumblrResponse makeRequestAndRespondToSuccessOrFailure(Invocation.Builder invocationBuilder) {
+        Response response = invocationBuilder.get();
+
         if (response.getStatus() == 200) {
             TumblrSuccessResponse successResponse = response.readEntity(TumblrSuccessResponse.class);
 
             return successResponse;
         } else {
+            System.out.println(response.getHeaders().toString());
+
             TumblrFailureResponse failResponse = response.readEntity(TumblrFailureResponse.class);
             System.out.println(failResponse.getMeta().getMessage());
 
             return failResponse;
         }
+    }
+
+    private WebTarget target(String pathString) {
+        return client.target(BASE_API_URL)
+                .queryParam("api_key", AUTHENTICATION)
+                .path(pathString);
     }
 }
