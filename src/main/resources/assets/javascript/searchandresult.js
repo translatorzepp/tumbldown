84465ab792ef7d1db631ac06cc745d0ab9bbe49c
zpalmer <zoepalmer2014@u.northwestern.edu>
2018-09-19 13:48:15 -0500

    WIP start spiking out receiving chunked output for search results

--- src/main/resources/assets/javascript/searchandresult.js
+++ src/main/resources/assets/javascript/searchandresult.js
@@ -1,3 +1,28 @@
 function showSearchSpinner() {
     document.getElementById('searching-spinner').removeAttribute('hidden');
 }
+
+function hideSearchSpinner() {
+    document.getElementById('searching-spinner').setAttribute('hidden', 'hidden');
+}
+
+function search() {
+    showSearchSpinner();
+
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", "/likes/chunked?blogName=ritterssport", true)
+    xhr.onprogress = function () {
+        document.getElementById('results').innerHTML = xhr.responseText;
+    }
+    xhr.onload = function () {
+        hideSearchSpinner()
+    }
+    .onerror = function () {
+        console.log("** An error occurred during search **");
+        hideSearchSpinner()
+    }
+    .onabort = function() {
+        hideSearchSpinner()
+    }
+    xhr.send()
+}
