fd70bbc95f1e04b208556b66a85a427c215321be
zpalmer <zoepalmer2014@u.northwestern.edu>
2018-09-25 12:30:02 -0500

    Cleanup: remove unused classes and methods

--- src/main/java/zpalmer/tumbldown/resources/PostsResource.java
+++ src/main/java/zpalmer/tumbldown/resources/PostsResource.java
@@ -102,46 +102,6 @@ public class PostsResource {
         }
     }
 
-    protected LinkedList<Post> searchForLikes(String blogName,
-                                              String searchText,
-                                              Long likedBeforeTimestampSeconds,
-                                              Long likedAfterTimestampSeconds,
-                                              LinkedList<Post> posts) {
-        boolean morePosts = true;
-        while (morePosts) {
-
-            TumblrResponse response = tumblrClient.getLikes(blogName, likedBeforeTimestampSeconds);
-            if (response instanceof TumblrSuccessResponse) {
-                TumblrSuccessResponse success = (TumblrSuccessResponse) response;
-                posts.addAll(success.getPosts());
-
-                Long lastLikedBeforeTimestampSeconds = posts.getLast().getLikedAt();
-
-                if (lastLikedBeforeTimestampSeconds.equals(likedBeforeTimestampSeconds) || lastLikedBeforeTimestampSeconds < likedAfterTimestampSeconds)
-                {
-                    morePosts = false;
-                } else {
-                    likedBeforeTimestampSeconds = lastLikedBeforeTimestampSeconds;
-                }
-            } else {
-                int tumblrErrorStatusCode = response.getMeta().getStatus();
-                String errorDetails;
-                if (tumblrErrorStatusCode == 403) {
-                    errorDetails = blogName + "'s likes are not public.";
-                } else if (tumblrErrorStatusCode == 404) {
-                    errorDetails = blogName + " does not exist.";
-                } else if(tumblrErrorStatusCode >= 500) {
-                    errorDetails= "Tumblr is down or unreachable.";
-                } else {
-                    errorDetails = "Unknown error: " + tumblrErrorStatusCode + ".";
-                }
-                throw new WebApplicationException(errorDetails, tumblrErrorStatusCode);
-            }
-        }
-
-        return filterPostsBySearchString(posts, Optional.ofNullable(searchText));
-    }
-
     protected LinkedList<Post> filterPostsBySearchString(LinkedList<Post> posts, Optional<String> searchText) {
         if (searchText.isPresent()) {
             posts.removeIf(post -> !post.containsText(searchText.get()));
